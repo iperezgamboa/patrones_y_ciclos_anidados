3882af3bb4303206b2179e013412497fccd5523f
Ignacio Pèrez Gamboa <i.perezgamboa@gmail.com>
2020-08-25 23:39:25 -0400

    patrones_y_ciclos_anidados

---
+++ numeros.rb
@@ -0,0 +1,6 @@
+n = ARGV[0].to_i
+
+    (n-4).times do
+        print "1 12 123 1234 12345"
+    end 
+    print "\n"
---
+++ patrones.rb
@@ -0,0 +1,228 @@
+#EJERCICIO A
+
+    n = ARGV[0].to_i
+
+    n.times do
+        print "*"
+    end 
+    print "\n"
+
+    (n-2).times do
+        print "*   *" 
+        puts " "
+    end 
+
+    n.times do
+        print "*"
+    end 
+    print "\n"
+
+
+#Ejercicio B esta es la i 
+# n = ARGV[0].to_i
+
+# n.times do
+#     print "*"
+# end 
+# print "\n"
+
+# (n-2).times do
+#     n.times do |j|
+#         if j == (n/2)
+#             print "*"
+#         else 
+#             print " "
+#         end
+#     end 
+#     puts 
+# end 
+        
+# n.times do 
+#     print "*"
+# end 
+
+
+# n = ARGV[0].to_i
+
+# n.times do |i|
+#     if i%9 == 0 || i%9 == 3 || i%9 == 6
+#         print ""
+#     elsif i%9 == 1 || i%9 == 4 || i%9 == 7
+#         print ""
+#     else i%9 == 2 || i%9 == 5 || i%9 == 8
+#         print ""
+#     end 
+# end 
+
+# puts ''
+
+
+#Ejercicio c  letra z 
+
+# n = ARGV[0].to_i
+
+# n.times do
+#     print "*"
+# end 
+# print "\n"
+
+# (1).times do
+#     print "   * " 
+#     puts " "
+# end 
+
+# (1).times do
+#     print "  * " 
+#     puts " "
+# end 
+
+# (1).times do
+#     print " * " 
+#     puts " "
+# end 
+
+# n.times do
+#     print "*"
+# end 
+# print "\n"
+
+
+#EJERCICIO LETRA X   
+
+# n = ARGV[0].to_i
+
+# 1.times do
+#     print "*   *"
+# end 
+# print "\n"
+
+# (1).times do
+#     print " * * " 
+#     puts " "
+# end 
+
+# (1).times do
+#     print "  * " 
+#     puts " "
+# end 
+
+# (1).times do
+#     print " * * " 
+#     puts " "
+# end 
+
+# 1.times do
+#     print "*   *"
+# end 
+# print "\n"
+
+
+#EJERCICIO Metodo numero_cero(n)
+
+# n = ARGV[0].to_i
+
+# n.times do
+#     print "*"
+# end 
+# print "\n"
+
+# (n-4).times do
+#     print "**  *"
+# end 
+# print "\n"
+
+# (n-2).times do
+#     print "* "
+# end 
+# print "\n"
+
+# (n-4).times do
+#     print "*  **"
+# end 
+# print "\n"
+
+# (n).times do
+#     print "*"
+# end 
+# print "\n"
+
+
+
+
+
+#EJERCICIO metodo
+
+
+# Metodo Navidad (n)
+
+# n = ARGV[0].to_i
+
+# (n+2).times do |i|
+#     if i%7 == 3
+#         print '*'
+#     else
+#         print ' '
+#     end
+# end
+
+# puts
+
+# (n+2).times do |i|
+#     if i%7 == 2 || i%7 == 4
+#         print '*'
+#     else
+#         print ' '
+#     end
+# end
+
+# puts
+
+# (n+2).times do |i|
+#     if i%7 == 1 || i%7 == 3 || i%7 == 5
+#         print '*'
+#     else
+#         print ' '
+#     end
+# end
+
+# puts
+
+# (n+2).times do |i|
+#     if i%2 == 0
+#         print '*'
+#     else
+#         print ' '
+#     end
+# end
+
+# puts
+
+# (n+2).times do |i|
+#     if i%7 == 3
+#         print '*'
+#     else
+#         print ' '
+#     end
+# end
+
+# puts
+
+# (n+2).times do |i|
+#     if i%7 == 3
+#         print '*'
+#     else
+#         print ' '
+#     end
+# end
+
+# puts
+
+# (n+2).times do |i|
+#     if i%2 == 0
+#         print ' '
+#     else
+#         print '*'
+#     end
+# end
+
+# puts
